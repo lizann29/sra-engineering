6a8979023d93591a8e3b590808ada63a054d9b7c
lizininua <lizininua2001@gmail.com>
2025-05-30 00:47:09 +0400

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script lang="ts">
+    import { base} from '$app/paths'
+</script>
+
+<a href="{base}/"> home</a>
+<a href="{base}/about">go about</a>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import { base} from '$app/paths'
+</script>
+
+<h2>about</h2>
+<a href="{base}/">go back home</a>

3ed3b8a41197bc7b4cf0f782825215b270318ede
lizininua <lizininua2001@gmail.com>
2025-05-31 20:25:41 +0400

    Refactor routes and update Vite base configuration
    Removed the home page Svelte file and added a new projects page with placeholder content. Updated Vite configuration to set a base path dynamically based on the build mode.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default defineConfig(({ mode }) => ({
+	plugins: [sveltekit()],
+	base: mode === 'production' ? '/sra-engineering/' : ''
+}));

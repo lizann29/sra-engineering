cf1ef05588fc229d2a9d403673835415fefa963b
lizininua <lizininua2001@gmail.com>
2025-05-31 18:32:52 +0400

    Add About Us, Services, and Contact Pages with Styling
    Introduce core components including About Us, Services, and Contact sections with modern styled layouts and animations. Add static assets for images and logos, implement navigation updates, and configure dependencies for Tailwind CSS and Svelte transitions.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { goto } from '$app/navigation';
-
-    onMount(() => {
-        goto('/home');
-    });
+    import Background from "$lib/components/background/Background.svelte";
+    import AboutUs from "$lib/components/about-us/AboutUs.svelte";
+    import Advantages from "$lib/components/advantages/Advantages.svelte";
+    import Banner from "$lib/components/banner/Banner.svelte";
+    import Services from "$lib/components/services/Services.svelte";
 </script>
+<div class="page">
+    <Background/>
+    <Banner/>
+    <Services/>
+    <AboutUs/>
+    <Advantages/>
+</div>
+
+<style lang="scss">
+  .page{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+</style>
